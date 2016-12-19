5bb23a5f9c37b48192fd35dd67b207d9c78158bf
antoniogarmendia <antonio.agj@gmail.com>
2016-12-19 09:24:04 +0100

    Minor

--- org.miso.ecore.statistics/src/org/miso/ecore/statistics/handlers/EcoreMMHandler.java
+++ org.miso.ecore.statistics/src/org/miso/ecore/statistics/handlers/EcoreMMHandler.java
@@ -6,7 +6,6 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.miso.ecore.statistics.dialogs.EcoreStatisticsDialog;
-import org.eclipse.jface.dialogs.MessageDialog;
 
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
